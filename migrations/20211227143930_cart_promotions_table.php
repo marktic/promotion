61d004a83e708045efc9de16820ba2fd65229d14
Gabriel Solomon <hello@gabrielsolomon.ro>
2021-12-28 11:29:38 +0200

    feat: Add cart promotion table definition

--- migrations/20211227143930_cart_promotions_table.php
+++ migrations/20211227143930_cart_promotions_table.php
@@ -19,5 +19,37 @@ final class CartPromotionsTable extends AbstractMigration
      */
     public function change(): void
     {
+        $table_name = 'mkt_promotions';
+        $exists = $this->hasTable($table_name);
+        if ($exists) {
+            return;
+        }
+        $table = $this->table($table_name);
+        $table
+            ->addColumn('pool_id', 'integer', ['null' => true])
+            ->addColumn('pool', 'string', ['null' => true])
+            ->addColumn('code', 'string', ['limit' => 255, 'null' => false])
+            ->addColumn('name', 'string', ['limit' => 255, 'null' => false])
+            ->addColumn('priority', 'integer', [])
+            ->addColumn('exclusive', 'integer', ['limit' => 1, 'null' => false, 'default' => 1])
+            ->addColumn('usage_limit', 'integer', ['null' => true])
+            ->addColumn('used', 'integer', [])
+            ->addColumn('valid_from', 'datetime', ['null' => true])
+            ->addColumn('valid_to', 'datetime', ['null' => true])
+            ->addColumn('updated_at', 'timestamp', [
+                'default' => 'CURRENT_TIMESTAMP',
+                'update' => 'CURRENT_TIMESTAMP',
+            ])
+            ->addColumn('created_at', 'timestamp', [
+                'default' => 'CURRENT_TIMESTAMP',
+            ]);
+
+        $table
+            ->addIndex(['pool_id'])
+            ->addIndex(['pool'])
+            ->addIndex(['priority'])
+            ->addIndex(['code'], ['unique' => true]);
+
+        $table->save();
     }
 }
