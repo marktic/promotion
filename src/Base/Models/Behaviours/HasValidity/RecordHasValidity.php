1abdd1218a9a12e707c895b94eb7f4f7d8f6d4be
Gabriel Solomon <hello@gabrielsolomon.ro>
2022-01-05 11:43:48 +0200

    feat: add promotion code controller and form

--- src/Base/Models/Behaviours/HasValidity/RecordHasValidity.php
+++ src/Base/Models/Behaviours/HasValidity/RecordHasValidity.php
@@ -2,6 +2,7 @@
 
 namespace Marktic\Promotion\Base\Models\Behaviours\HasValidity;
 
+use Carbon\Carbon;
 use DateTime;
 
 trait RecordHasValidity
@@ -10,6 +11,9 @@ trait RecordHasValidity
 
     protected ?string $valid_to;
 
+    /**
+     * @return DateTime|Carbon|null
+     */
     public function getValidFrom(): ?DateTime
     {
         return $this->getPropertyValue('valid_from');
@@ -23,6 +27,9 @@ trait RecordHasValidity
         $this->setPropertyValue('valid_from', $date);
     }
 
+    /**
+     * @return DateTime|Carbon|null
+     */
     public function getValidTo(): ?DateTime
     {
         return $this->getPropertyValue('valid_to');
