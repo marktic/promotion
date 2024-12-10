06bf826b748f10d3e62f6458122048920f0326f4
Gabriel Solomon <hello@gabrielsolomon.ro>
2024-12-10 18:26:16 +0200

    feat: initial add price calculator

--- migrations/20241113103962_mkt_promotion_gift_card_products_description.php
+++ migrations/20241113103962_mkt_promotion_gift_card_products_description.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use Phinx\Migration\AbstractMigration;
 
-final class MktPromotionGiftCardProductsName extends AbstractMigration
+final class MktPromotionGiftCardProductsDescription extends AbstractMigration
 {
     /**
      * Change Method.
